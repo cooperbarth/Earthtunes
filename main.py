87a336be089951fcf171c30b81df68cdf15158ab
coopahbah <35340310+coopahbah@users.noreply.github.com>
2018-07-16 16:19:14 -0500

    did a lot of cuul shit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy
 import urllib2
 import matplotlib.pyplot as plt
 import random
+import re
 
 plt.style.use(['dark_background'])
 
@@ -95,6 +96,8 @@ def slideSeek(instance,touch):
 			sound.play()
 			sound.seek((slider.value/slider.max)*sound.length)
 
+			
+			
 #getSoundAndGraph: Script that pulls data and processes into image and audio
 def getSoundAndGraph(locate, date, time, duration, AF, FA):
 	halfpi = 0.5*numpy.pi
@@ -102,6 +105,7 @@ def getSoundAndGraph(locate, date, time, duration, AF, FA):
 	disploc = locate
 	time = time + ':00'
 	
+	#setting location and station based on user input
 	if locate == 'Ryerson (IL,USA)':
 		soundname = 'ryerson'
 		station = "L44A"
@@ -158,51 +162,21 @@ def getSoundAndGraph(locate, date, time, duration, AF, FA):
 		location = "--"
 		channel = "BHZ"
 		
+	#getting data from online
 	print "Getting data from",disploc,'on',date
-	
-	# Example dates:
-		# a specific random day (with 2 thunderstorms):
-			# date = "2016-07-24"
-		# Alaska M6.2 & Alaska M6.2 
-			#date = "2017-05-01"
-		# M5.8 Montana and M6.5 Philippines 
-			#date = "2017-07-06"
-		# Oklahoma M4.2
-			#date = "2017-07-14"
-
-	# 6 different time series acceleration factors ("stretch" factors in the frequency domain).
-	# only one of them is used in line 99 and 110.
-	if AF == '':
-		bandsHZ = 400
-	else:
-		bandsHZ = float(AF)
-	
-	#bandstupto50Hz = 160
-	#bandstupto20Hz = 400
-	#bandstupto10Hz = 800
-	#bandstupto5Hz = 1600
-	#bandstuptohalfHz = 16000
-	#bandstuptotenthHz = 64000
-
-	# request data from IRIS' timeseries web service
 	type = net + "&sta=" + station + "&loc=" + location + "&cha=" + channel
 	when = "&starttime=" + date + "T" + time + "&duration=" + duration
 	url = "http://service.iris.edu/irisws/timeseries/1/query?net=" + type + when + "&demean=true&scale=auto&output=ascii1"
-
 	print "requesting data from IRIS...please be patient..."
-	#sm.get_screen('Loading Screen').message.text = "requesting data from IRIS...please be patient..."
 	ws = urllib2.urlopen(url)
 	print "loading data ..."
-	#sm.get_screen('Loading Screen').message.text = 'loading data...'
 	df = ws.read()
 	print "processing data..."
-	#sm.get_screen('Loading Screen').message.text = 'processing data...'
 	dflines = df.split('\n')
 	   
+	#getting the data from the doc
 	head = dflines[0]
-	# sampling rate in data:
 	fsps = numpy.float(head.split()[4])
-	# total number of samples in data:
 	tot = numpy.float(head.split()[2])
 	sound = []
 	maxAmp = 0
@@ -215,42 +189,48 @@ def getSoundAndGraph(locate, date, time, duration, AF, FA):
 			tot = tot + numpy.float(l.split()[2])
 	sound = numpy.asarray(sound)
 
-	# use a fixed amplitude scale for seismograms with physical (m/s) y-axis units (use "scale=AUTO" in web request)
-	# calculate signal level (in physical units (m/s)) to which you want sound to scale quasi-linearly (one-third of the maximum signal)
+	#setting amplitude and frequency based on user input
+	if AF == '0.1 Hz':
+		bandsHZ = 64000
+	elif AF == '0.5 Hz':
+		bandsHZ = 16000
+	elif AF == '5 Hz':
+		bandsHZ = 1600
+	elif AF == '10 Hz':
+		bandsHZ = 800
+	elif AF == '50 Hz':
+		bandsHZ = 160
+	else:
+		bandsHZ = 400
+		
 	if FA == '':
 		fixedamp = maxAmp / 3.
 	else:
 		fixedamp = float(FA)
 	
-	# duration of data (in hours):
+	#creating the sound file
 	realduration = (tot/fsps)/3600.
 	print "original duration = %7.2f hours" % realduration
 	hours = numpy.linspace(0,realduration,tot)
-
 	soundduration = tot/(fsps*bandsHZ)
 	print "max 20Hz wav file duration = %8.1f seconds" % (soundduration)
-
 	mxs = 1.01*numpy.max(sound)
 	mns = 1.01*numpy.min(sound)
-
-	# An arctan() function is used to compress sound
 	scaledsound = (2**31)*numpy.arctan(sound/fixedamp)/halfpi
 	s32 = numpy.int32(scaledsound)
-
-	# filename explanation: numbers between underscores are freq range (in mHz) that's sonified in audible range.
 	ssps = bandsHZ * fsps
 	wavfile.write(soundname + "_400_20000.wav",ssps,s32)
-
-	axes(xlim=[0,realduration], ylim=[1000*mns,1000*mxs], xlabel="Time since "+time+ " (hours)",ylabel="Ground Velocity (mm/s)", title=locate+', '+date)
-	# plot y in mm (or mm/s) rather than m:
-	plot(hours,1000.*sound)
 	
+	#plotting the graph
+	axes(xlim=[0,realduration], ylim=[1000*mns,1000*mxs], xlabel="Time since "+time+ " (hours)",ylabel="Ground Velocity (mm/s)", title=locate+', '+date)
+	plot(hours,1000.*sound)
 	axishours = [time]
 	axis([hours[0],hours[-1],-3000.*fixedamp,3000.*fixedamp])
 	savefig(soundname + ".png")
 	
 	return soundname
 
+#isdigit function that works with scientific notation
 def isNumber(number):
 	try:
 		float(number)
@@ -258,6 +238,9 @@ def isNumber(number):
 		return False
 	return True		
 	
+	
+	
+#define labels with different colored backgrounds
 class BlueLabel(Label):
 	def on_size(self, *args):
 		self.canvas.before.clear()
@@ -271,10 +254,56 @@ class WhiteLabel(Label):
 		with self.canvas.before:
 			Color(1, 1, 1, 1)
 			Rectangle(pos=self.pos, size=self.size)
+
+			
+#textinputs that can only accept certain arguments			
+class FloatInput(TextInput):
+	pat = re.compile('[^0-9]')
+	def insert_text(self, substring, from_undo=False):
+		pat = self.pat
+		if self.text.count('.') > 0:
+			s = re.sub(pat, '', substring)
+		else:
+			s = '.'.join([re.sub(pat, '', s) for s in substring.split('.', 1)])
+		return super(FloatInput, self).insert_text(s, from_undo=from_undo)
 		
-geofacts = ['0','1','2','3','4','5','6','7','8','9']
+class DateInput(TextInput):
+	pat = re.compile('[^0-9]')
+	def insert_text(self, substring, from_undo=False):
+		pat = self.pat
+		if self.text.count('-') > 1:
+			s = re.sub(pat, '', substring)
+		else:
+			s = '-'.join([re.sub(pat, '', s) for s in substring.split('-', 1)])
+		return super(DateInput, self).insert_text(s, from_undo=from_undo)
+
+class TimeInput(TextInput):
+	pat = re.compile('[^0-9]')
+	def insert_text(self, substring, from_undo=False):
+		pat = self.pat
+		if self.text.count(':') > 0:
+			s = re.sub(pat, '', substring)
+		else:
+			s = ':'.join([re.sub(pat, '', s) for s in substring.split(':', 1)])
+		return super(TimeInput, self).insert_text(s, from_undo=from_undo)
 		
-#toDisplay: transitions to Loading Screen; calls earthtunes and reloads sound/image to match
+		
+#list of geology facts for use on the loading screen
+geofacts = ['0',
+			'1',
+			'2',
+			'3',
+			'4',
+			'5',
+			'6',
+			'7',
+			'8',
+			'9']
+		
+		
+		
+		
+#screen transition functions
 def toDisplay(instance):
 	global geofacts
 
@@ -334,7 +363,6 @@ def toDisplay(instance):
 	sm.get_screen('Loading Screen').message.text= "Loading data from " + sm.get_screen('Input Screen').location.text + '\n\n\n\n\n' + geofacts[random.randint(0,9)]
 	sm.current = 'Loading Screen'
 
-#toInput: transitions back to input screen; stops sound, resets picture
 def toInput(instance):
 	if sound.state is 'play':
 		sound.stop()
@@ -365,6 +393,9 @@ def toAdvanced(instance):
 	sm.transition.direction = 'left'
 	sm.current = 'Advanced Screen'
 	
+	
+	
+#screen classes
 class ChooseScreen(Screen):
 	def __init__(self, **kwargs):
 		super(ChooseScreen, self).__init__(**kwargs)
@@ -388,7 +419,9 @@ class ChooseScreen(Screen):
 		self.location.font_size = 20
 		self.layout.add_widget(self.location)
 		self.layout.add_widget(Label(size_hint = (1, 0.702)))
-		self.select = Button(text='Select', font_size=20, size_hint=(1,0.11), bold=True)
+		
+		self.layout.add_widget(WhiteLabel(size_hint=(1,0.001)))
+		self.select = Button(text='Select', font_size=20, size_hint=(1,0.109), bold=True)
 		self.select.bind(on_release=Selected)
 		self.layout.add_widget(self.select)
 		self.add_widget(self.layout)
@@ -399,84 +432,94 @@ sm.add_widget(choose)
 class AdvancedScreen(Screen):
 	def __init__(self, **kwargs):
 		super(AdvancedScreen, self).__init__(**kwargs)
-		self.box = BoxLayout(orientation='vertical')
-		self.title = BlueLabel(text="SonifyMe", size_hint=(1,0.109), valign='middle', bold=True, halign = 'center')
-		self.title.font_size = self.title.height/3
-		self.title.bind(size=self.title.setter('text_size'))
-		self.box.add_widget(self.title)
-		self.box.add_widget(WhiteLabel(size_hint=(1,0.001)))
-		
-		self.layout = GridLayout(cols=2, size_hint=(1,0.8))
-		self.layout.add_widget(Label())
-		self.layout.add_widget(Label())
-		self.layout.add_widget(Label(text='Acceleration Factor\n(160-64000)', halign='center'))
-		self.aFactor = TextInput(multiline=False)
-		self.layout.add_widget(self.aFactor)
-		self.layout.add_widget(Label())
-		self.layout.add_widget(Label())
-		self.layout.add_widget(Label(text='Fixed Amplitude'))
-		self.fixedAmp = TextInput(multiline=False)
-		self.layout.add_widget(self.fixedAmp)
-		self.layout.add_widget(Label())
-		self.layout.add_widget(Label())
-		
-		self.returnbutton = Button(text='Return', size_hint=(1,0.2))
-		self.returnbutton.bind(on_release=toInputSimple)
-		
-		self.box.add_widget(self.layout)
-		self.box.add_widget(self.returnbutton)
-		
-		self.add_widget(self.box)
-
-class InputScreen(Screen):
-    
-	def __init__(self, **kwargs):
-		global choose
-	
-		super(InputScreen, self).__init__(**kwargs)
 		self.layout = BoxLayout(orientation='vertical')
-		
 		self.title = BlueLabel(text="SonifyMe", size_hint=(1,0.109), valign='middle', bold=True, halign = 'center')
 		self.title.font_size = self.title.height/3
 		self.title.bind(size=self.title.setter('text_size'))
 		self.layout.add_widget(self.title)
 		self.layout.add_widget(WhiteLabel(size_hint=(1,0.001)))
 		
-		self.grid0 = GridLayout(cols=2, rows=1, size_hint=(1, 0.189))
+		self.aFactor = Spinner(
+				text='Acceleration Factor:',
+				values=('0.1 Hz', '0.5 Hz', '5 Hz', '10 Hz', '20 Hz', '50 Hz'),
+				size_hint = (1,0.08),
+				sync_height=True	
+				)	
+		self.layout.add_widget(self.aFactor)
+		self.layout.add_widget(Label(size_hint=(1,0.49)))
+		
+		self.layout.add_widget(WhiteLabel(size_hint=(1,0.001)))
+		self.grid = GridLayout(cols=2, size_hint=(1, 0.149))
+		self.fixedlabel = Label(text='Fixed Amplitude:')
+		self.fixedlabel.font_size = self.fixedlabel.height/5
+		self.grid.add_widget(self.fixedlabel)
+		self.fixedAmp = TextInput(multiline=False)
+		self.grid.add_widget(self.fixedAmp)
+		self.layout.add_widget(self.grid)
+		
+		self.layout.add_widget(WhiteLabel(size_hint=(1,0.001)))
+		self.returnbutton = Button(text='Return', size_hint=(1,0.149))
+		self.returnbutton.font_size=self.returnbutton.height/5
+		self.returnbutton.bind(on_release=toInputSimple)
+		self.layout.add_widget(self.returnbutton)
+		
+		self.add_widget(self.layout)
+
+class InputScreen(Screen):
+	def __init__(self, **kwargs):
+		global choose
+	
+		super(InputScreen, self).__init__(**kwargs)
+		self.layout = BoxLayout(orientation='vertical')
+		
+		self.title = BlueLabel(text="SonifyMe", size_hint=(1,0.1085), valign='middle', bold=True, halign = 'center')
+		self.title.font_size = self.title.height/3
+		self.title.bind(size=self.title.setter('text_size'))
+		self.layout.add_widget(self.title)
+		self.layout.add_widget(WhiteLabel(size_hint=(1,0.0015)))
+		
+		self.grid0 = GridLayout(cols=2, rows=1, size_hint=(1, 0.1885))
 		self.LocationLabel = Label(text='Location:', valign='middle')
 		self.LocationLabel.font_size = self.LocationLabel.height/5
 		self.grid0.add_widget(self.LocationLabel)
-		self.location = Button(text=choose.location.text, font_size=self.height/7, valign='middle')
+		self.location = Button(text=choose.location.text, valign='middle')
+		self.location.font_size=self.location.height/5
 		self.location.bind(on_release=toChoose)
 		self.grid0.add_widget(self.location)
 		self.layout.add_widget(self.grid0)
-		self.layout.add_widget(WhiteLabel(size_hint=(1,0.001)))
+		self.layout.add_widget(WhiteLabel(size_hint=(1,0.0015)))
 		
-		self.grid1 = GridLayout(cols=2, rows=1, size_hint=(1, 0.189))
-		self.grid1.add_widget(Label(text='Date (YYYY-MM-DD):', font_size=self.height/5, valign='middle'))
-		self.date = TextInput(multiline=False, font_size = self.height/7)
+		self.grid1 = GridLayout(cols=2, rows=1, size_hint=(1, 0.1885))
+		self.datelabel = Label(text='Date (YYYY-MM-DD):')
+		self.datelabel.font_size = self.datelabel.height/5
+		self.datelabel.valign = 'middle'
+		self.grid1.add_widget(self.datelabel)
+		self.date = DateInput(multiline=False)
+		self.date.font_size = self.date.height/5
 		self.grid1.add_widget(self.date)
 		self.layout.add_widget(self.grid1)
-		self.layout.add_widget(WhiteLabel(size_hint=(1,0.001)))
+		self.layout.add_widget(WhiteLabel(size_hint=(1,0.0015)))
 		
-		self.grid2 = GridLayout(cols=2, rows=1, size_hint=(1,0.189))
+		self.grid2 = GridLayout(cols=2, rows=1, size_hint=(1,0.1885))
 		self.grid2.add_widget(Label(text='Start Time (HH:MM):', font_size=self.height/5, valign='middle'))
-		self.startTime = TextInput(multiline=False, font_size = self.height/7)
+		self.startTime = TimeInput(multiline=False)
+		self.startTime.font_size = self.startTime.height/5
 		self.grid2.add_widget(self.startTime)
 		self.layout.add_widget(self.grid2)
-		self.layout.add_widget(WhiteLabel(size_hint=(1,0.001)))
+		self.layout.add_widget(WhiteLabel(size_hint=(1,0.0015)))
 		
-		self.grid3 = GridLayout(cols=2, rows=1, size_hint=(1,0.189))
+		self.grid3 = GridLayout(cols=2, rows=1, size_hint=(1,0.1885))
 		self.grid3.add_widget(Label(text='Duration (minutes):', font_size=self.height/5, valign='middle'))
-		self.duration = TextInput(multiline=False, font_size = self.height/7)
+		self.duration = FloatInput(multiline=False)
+		self.duration.font_size = self.duration.height/5
 		self.grid3.add_widget(self.duration)
 		self.layout.add_widget(self.grid3)
-		self.layout.add_widget(WhiteLabel(size_hint=(1,0.001)))
+		self.layout.add_widget(WhiteLabel(size_hint=(1,0.0015)))
 		
-		self.advanced = Button(text='Advanced Options', font_size = self.height/7, size_hint=(1, 0.039), valign='middle', background_color=(0, 0, 1, 1))
+		self.advanced = Button(text='Advanced Options', font_size = self.height/7, size_hint=(1, 0.0385), valign='middle', background_color=(0, 0, 1, 1))
 		self.advanced.bind(on_release=toAdvanced)
 		self.layout.add_widget(self.advanced)
-		self.layout.add_widget(WhiteLabel(size_hint=(1,0.001)))
+		self.layout.add_widget(WhiteLabel(size_hint=(1,0.0015)))
 		
 		self.button = Button(text='Submit', font_size=self.height/7, size_hint=(1,0.089), valign='middle')
 		self.button.bind(on_release=toDisplay)
@@ -529,7 +572,6 @@ class LoadingScreen(Screen):
 			sm.current = 'Display Screen'
 
 class Error404(Screen):
-	
 	def __init__(self, **kwargs):
 		super(Error404, self).__init__(**kwargs)
 		self.layout = GridLayout(cols=1)
@@ -542,7 +584,6 @@ class Error404(Screen):
 		self.add_widget(self.layout)
 		
 class DisplayScreen(Screen):
-
 	def __init__(self, **kwargs):
 		super(DisplayScreen, self).__init__(**kwargs)
 		self.layout = BoxLayout(orientation='vertical')
@@ -573,6 +614,7 @@ class DisplayScreen(Screen):
 		self.layout.add_widget(self.button)
 		self.add_widget(self.layout)
 
+		
 # Create screens and add to manager
 loading = LoadingScreen(name='Loading Screen')
 display = DisplayScreen(name='Display Screen')
