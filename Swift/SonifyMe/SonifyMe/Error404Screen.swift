c0e03414379f746c59bfe1d567eb351001407b7c
Cooper Finn Barth <CooperBarth2021@u.northwestern.edu>
2018-07-31 13:42:26 -0500

    added title, cleaned up

--- Swift/SonifyMe/SonifyMe/Error404Screen.swift
+++ Swift/SonifyMe/SonifyMe/Error404Screen.swift
@@ -16,25 +16,4 @@ class Error404Screen : ViewController {
         InputErrorLabel.text = "Error 404: The Requested\n Data is Unavailable"
         self.showAnimate()
     }
-    
-    func showAnimate() {
-        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
-        self.view.alpha = 0.0;
-        UIView.animate(withDuration: 0.25, animations: {
-            self.view.alpha = 1.0
-            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
-            
-        });
-    }
-    
-    func removeAnimate() {
-        UIView.animate(withDuration: 0.25, animations: {
-            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
-            self.view.alpha = 0.0;
-        }, completion:{(finished : Bool)  in
-            if (finished) {
-                self.view.removeFromSuperview()
-            }
-        });
-    }
 }
--- Swift/SonifyMe/SonifyMe/ViewController.swift
+++ Swift/SonifyMe/SonifyMe/ViewController.swift
@@ -75,6 +75,27 @@ class ViewController: UIViewController, UITextFieldDelegate {
         view.endEditing(true)
     }
     
+    func showAnimate() {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            
+        });
+    }
+    
+    func removeAnimate() {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+        }, completion:{(finished : Bool)  in
+            if (finished) {
+                self.view.removeFromSuperview()
+            }
+        });
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
