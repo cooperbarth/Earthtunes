516729774602568606715575e401b0f8763c67e3
Cooper Finn Barth <CooperBarth2021@u.northwestern.edu>
2018-08-13 14:04:18 -0500

    started some suggestion screen stuff

--- Swift/SonifyMe/SonifyMe/SuggestionScreen.swift
+++ Swift/SonifyMe/SonifyMe/SuggestionScreen.swift
@@ -2,5 +2,17 @@ import Foundation
 import UIKit
 
 class SuggestionScreen : ViewController {
+    @IBOutlet weak var SuggestionView: UIView!
+    @IBOutlet weak var SuggestionScroll: UITableView!
+
+    @IBAction func ReturnButton(_ sender: Any) {
+        self.removeAnimate()
+    }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch: UITouch? = touches.first
+        if (touch?.view != SuggestionView && touch?.view != SuggestionScroll) {
+            self.removeAnimate()
+        }
+    }
 }
