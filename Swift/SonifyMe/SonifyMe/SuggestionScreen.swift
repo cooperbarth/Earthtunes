1b02f3b91a8c2b0b6d6a3c04ef6c7b2859c4c215
Cooper Barth <coop10101@gmail.com>
2018-08-23 16:03:37 -0500

    downgraded to swift 4.1 (apple rejects apps built on beta xcode apparently)

--- Swift/SonifyMe/SonifyMe/SuggestionScreen.swift
+++ Swift/SonifyMe/SonifyMe/SuggestionScreen.swift
@@ -182,7 +182,7 @@ extension SuggestionScreen {
     @objc func longPress(_ longPressGestureRecognizer: UILongPressGestureRecognizer) {
         let touchPoint = longPressGestureRecognizer.location(in: self.SuggestionScroll)
         if let indexPath = SuggestionScroll.indexPathForRow(at: touchPoint) {
-            if (longPressGestureRecognizer.state == UIGestureRecognizer.State.began) {
+            if (longPressGestureRecognizer.state == UIGestureRecognizerState.began) {
                 view.endEditing(true)
                 ud.set(indexPath.row, forKey: "Long-Pressed Index")
                 showPopup(name: "Info Screen")
